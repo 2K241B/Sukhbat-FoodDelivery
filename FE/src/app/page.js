648776ccd489085e9566c2ab4d9773b54da8c89d
Ksubi666 <Ksubi666@gmail.com>
2024-09-03 21:20:48 +0800

    [FE] navbar without search logic

--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -1,8 +1,9 @@
 import Login from '@/components/Login';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="w-full">
       <Login />
     </main>
   );
