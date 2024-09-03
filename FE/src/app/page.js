7ef11b41fd2eda9c6d9e7f60e6cb0629c40e47c6
Ksubi666 <Ksubi666@gmail.com>
2024-09-04 01:25:14 +0800

    [FE] User Page Component add

--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -2,14 +2,16 @@ import ForgetPass from '@/components/ForgetPass';
 import Login from '@/components/Login';
 import NotFoundPage from '@/components/NotFoundPage';
 import SignUp from '@/components/SignUp';
+import UserPage from '@/components/UserPage';
 
 export default function Home() {
   return (
-    <main className="w-full bg-[#ccc] h-screen">
+    <main className="w-full bg-[#404040] h-screen">
       {/* <Login /> */}
       {/* <SignUp /> */}
       {/* <ForgetPass /> */}
       {/* <NotFoundPage /> */}
+      {/* <UserPage /> */}
     </main>
   );
 }
