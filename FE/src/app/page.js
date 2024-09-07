1949a9b339ad3a882a3c22efabd875829a46f54f
Ksubi666 <Ksubi666@gmail.com>
2024-09-08 02:22:53 +0800

    [FE] login logic refactor

--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -1,15 +1,11 @@
 import Feature from '@/components/Feature';
-import Footer from '@/components/Footer';
-import ForgetPass from '@/components/ForgetPass';
-import Login from '@/components/Login';
-import NotFoundPage from '@/components/NotFoundPage';
-import SignUp from '@/components/SignUp';
-import UserPage from '@/components/UserPage';
+import HomePageMain from '@/components/HomePageMain';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center pt-[111px] pb-[75px]">
-      <Login />
+    <main>
+      <HomePageMain />
+      <Feature />
     </main>
   );
 }
