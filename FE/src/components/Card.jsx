67c4b24f6fa841fd589381b99a07dd7035eaf870
ksubi666 <ksubi666@gmail.com>
2024-09-10 15:05:34 +0800

    [FE] items refactor+

--- FE/src/components/Card.jsx
+++ FE/src/components/Card.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 
 const styles = {
-  container: 'flex flex-col gap-[14px]',
-  imgContainer: 'relative w-[282px] h-[186px] rounded-2xl',
+  container: 'flex flex-col gap-[14px] items-start',
+  imgContainer: 'relative w-[282px] min-h-[186px] rounded-2xl',
   discount:
     ' absolute rounded-2xl py-1 px-4 text-lg font-semibold text-white bg-[#18BA51] border-white border-[1px] flex justify-center items-center top-4 right-4',
   title: 'text-[20px] font-[590]',
