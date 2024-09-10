b04e647aa18ba71bf0e8ca85c683a9b86bab6b60
Ksubi666 <Ksubi666@gmail.com>
2024-09-11 03:15:20 +0800

    [FE] refactor

--- FE/src/app/layout.js
+++ FE/src/app/layout.js
@@ -14,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <div className="min-h-screen">
+          <Navbar />
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
