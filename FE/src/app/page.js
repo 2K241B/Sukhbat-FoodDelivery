4ebe9383dd23c52e8965fc029ad1dfd5fef15d8a
ksubi666 <ksubi666@gmail.com>
2024-09-10 14:29:08 +0800

    [FE] order detail dialog component (only ui) refactor

--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -1,11 +1,21 @@
 import Feature from '@/components/Feature';
 import HomePageMain from '@/components/HomePageMain';
+import OrderDetailDialog from '@/components/OrderDetailDialog';
+import food from '@/components/assets/cardFood.png';
 
 export default function Home() {
   return (
     <main>
       <HomePageMain />
       <Feature />
+      <div className="p-20">
+        <OrderDetailDialog
+          name="Өглөөний хоол"
+          imageSrc={food}
+          price={4800}
+          recipe="Хулуу, төмс, лууван , сонгино, цөцгийн тос, самрын үр  "
+        />
+      </div>
     </main>
   );
 }
--- FE/src/components/OrderDetailDialog.jsx
+++ FE/src/components/OrderDetailDialog.jsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Dialog,
   DialogClose,
@@ -13,6 +14,16 @@ import Image from 'next/image';
 import kk from '@/components/assets/deliveryZone.png';
 import PlusIcon from './icons/PlusIcon';
 import MinusIcon from './icons/MinusIcon';
+import { useEffect, useState } from 'react';
+const style = {
+  container: 'flex flex-col gap-[14px] items-start',
+  imgContainer: 'relative w-[282px] min-h-[186px] rounded-2xl',
+  discount:
+    ' absolute rounded-2xl py-1 px-4 text-lg font-semibold text-white bg-[#18BA51] border-white border-[1px] flex justify-center items-center top-4 right-4',
+  title: 'text-[20px] font-[590]',
+  price: 'text-[#18BA51] font-semibold text-lg',
+  salePrice: 'text-lg line-through',
+};
 
 const styles = {
   dialogContent: 'min-w-[981px] min-h-[564px] flex flex-row gap-[33px]',
@@ -22,19 +33,52 @@ const styles = {
   button: 'rounded-[10px] bg-[#18BA51] text-white h-10',
   submitButton: ' bg-[#18BA51] text-white rounded-[4px]',
   current: 'min-w-[254px] flex items-center justify-center font-semibold',
-  orts: 'text-[#767676] p-3 bg-[#F6F6F6] rounded-[8px] flex items-center justify-center',
+  recipe:
+    'text-[#767676] p-3 bg-[#F6F6F6] rounded-[8px] flex items-center justify-center',
   price: 'text-[18px] font-semibold text-[#18BA51]',
   buttonContainer: 'flex justify-between gap-5',
 };
 
-export const OrderDetailDialog = ({ name, price, orts, imageSrc }) => {
+export const OrderDetailDialog = ({
+  name,
+  price,
+  recipe,
+  imageSrc,
+  salePrice,
+}) => {
+  const [current, setCurrent] = useState(1);
+  useEffect(() => {
+    if (current < 1) return setCurrent(1);
+  }, [current]);
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Order Detail</Button>
+      <DialogTrigger>
+        <div className={style.container}>
+          <div className={style.imgContainer}>
+            <Image src={imageSrc} className="rounded-2xl " />
+            {salePrice && (
+              <div className={style.discount}>
+                {Math.floor(((salePrice - price) / salePrice) * 100)}%
+              </div>
+            )}
+          </div>
+
+          <div>
+            <h2 className={style.title}>{name}</h2>
+            <div className="flex gap-4">
+              {price && <p className={style.price}>{price}₮</p>}
+              {salePrice && <p className={style.salePrice}>{salePrice}₮</p>}
+            </div>
+          </div>
+        </div>
       </DialogTrigger>
       <DialogContent className={styles.dialogContent}>
-        <Image src={imageSrc} width={500} height={500} />
+        <Image
+          src={imageSrc}
+          width={500}
+          height={500}
+          style={{ objectFit: 'cover' }}
+        />
         <div className={styles.subContainer}>
           <div>
             <h2 className={styles.header}>{name}</h2>
@@ -42,15 +86,21 @@ export const OrderDetailDialog = ({ name, price, orts, imageSrc }) => {
           </div>
           <div className="flex flex-col gap-3">
             <h4 className={styles.subHeader}>Орц</h4>
-            <p className={styles.orts}>{orts}</p>
+            <p className={styles.recipe}>{recipe}</p>
           </div>
           <h4 className={styles.subHeader}>Тоо</h4>
           <div className={styles.buttonContainer}>
-            <Button className={styles.button}>
+            <Button
+              onClick={() => setCurrent(current - 1)}
+              className={styles.button}
+            >
               <MinusIcon />
             </Button>
-            <p className={styles.current}>1</p>
-            <Button className={styles.button}>
+            <p className={styles.current}>{current}</p>
+            <Button
+              onClick={() => setCurrent(current + 1)}
+              className={styles.button}
+            >
               <PlusIcon />
             </Button>
           </div>
