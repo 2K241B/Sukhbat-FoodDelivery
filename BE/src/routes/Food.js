83eff2ff1932a22a52d0b247f4e693f34f8613d1
Ksubi666 <Ksubi666@gmail.com>
2024-09-14 02:14:50 +0800

    [BE] get one food query

--- BE/src/routes/Food.js
+++ BE/src/routes/Food.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { createFood, getFoods } from '../controller/food.js';
+import { createFood, getFood, getFoods } from '../controller/food.js';
 
 const food = Router();
 
-food.post('/create', createFood).get('/getFoods', getFoods);
+food
+  .post('/create', createFood)
+  .get('/getFoods', getFoods)
+  .get('/getFood/:id', getFood);
 
 export default food;
