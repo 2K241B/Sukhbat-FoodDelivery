966a3d1c994da57f8069ce9ee882fbf941cf0c84
Ksubi666 <Ksubi666@gmail.com>
2024-09-14 01:38:09 +0800

    [BE] get all category query

--- BE/src/routes/Category.js
+++ BE/src/routes/Category.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { createCategory } from '../controller/category.js';
+import { createCategory, getCategories } from '../controller/category.js';
 
 const category = Router();
 
-category.post('/create', createCategory);
+category.post('/create', createCategory).get('/getCategories', getCategories);
 
 export default category;
