bb5b3442891df70afbe7e70b6593dc0b177e7711
ksubi666 <161238314+ksubi666@users.noreply.github.com>
2024-09-27 11:11:41 +0800

    [BE] refactor order query

--- BE/src/schema/order.js
+++ BE/src/schema/order.js
@@ -1,18 +1,32 @@
 import { model, Schema } from 'mongoose';
 
 const orderSchema = new Schema({
-  userId: String,
+  userId: {
+    type: Schema.ObjectId,
+    ref: 'user',
+    required: [true, 'userId is required'],
+  },
   orderNumber: Number,
-  foods: [],
+  foods: [
+    {
+      type: Schema.ObjectId,
+      ref: 'food',
+      required: [true, 'foods is required'],
+    },
+  ],
   totalPrice: Number,
-  process: { type: String, enum: [], default: '' },
-  createdDate: Date,
-  district: String,
-  Khoroo: String,
-  Apartment: String,
-  Discription: String,
-  PhoneNumber: String,
-  MethodOfPay: { type: String, enum: [], default: '' },
+  process: {
+    type: String,
+    enum: ['active', 'waiting', 'process', 'delivered'],
+    default: 'waiting',
+  },
+  createdDate: { type: Date, default: Date.now },
+  district: { type: String, required: [true, 'district is required'] },
+  Khoroo: { type: String, required: [true, 'khoroo is required'] },
+  Apartment: { type: String, required: [true, 'Apartment is required'] },
+  Description: { type: String, required: [true, 'description is required'] },
+  PhoneNumber: { type: String, required: [true, 'PhoneNumber is required'] },
+  MethodOfPay: { type: String, enum: ['cash', 'card'], default: 'cash' },
 });
 
 export const orderModel = model('order', orderSchema);
